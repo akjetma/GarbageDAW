bd498e86b77c8813530682d435389d120b8b8ba3
Adam Jetmalani <adamjetmalani@Adams-MacBook-Pro.local>
2019-06-18 19:35:58 -0700

    include old stuff in case i want to revert

--- src/crt_dreams/core.cljs
+++ src/crt_dreams/core.cljs
@@ -3,8 +3,76 @@
 
 (enable-console-print!)
 
+(def MAX_UINT32 4294967295)
+(def U32_MOD 4278190080)
+(def U32_RANGE (- MAX_UINT32 U32_MOD))
+
+(defn img-int->audio-float
+  [x]
+  (-> x
+      (- U32_MOD)   ; ignore alpha channel
+      (/ U32_RANGE) ; divide by max 24-bit number (rgb8)
+      (* 2)         ; multiply by 2...
+      (- 1)))       ; and shift down by one because audio floats E [-1, 1]
+
+(defn audio-float->img-int
+  [x]
+  (-> x
+      (+ 1)
+      (/ 2)
+      (* U32_RANGE)
+      (+ U32_MOD)))
+
+(defn image-array-to-audio-array
+  [image-array]
+  (js/Float32Array.from (js/Uint32Array. (.-buffer image-array)) img-int->audio-float))
+
+(defn audio-array-to-image-array
+  [audio-array]
+  (js/Uint8ClampedArray. (.-buffer (js/Uint32Array.from audio-array audio-float->img-int))))
+
 (defonce source-image (reagent/atom nil))
 (defonce crop-region (reagent/atom nil))
+
+(def pixels-per-second 1)
+(def num-channels 2)
+
+(defn img-data->audio-old
+  [audio-ctx img-data]
+  (let [frames-per-pixel (js/Math.floor (/ (.-sampleRate audio-ctx) pixels-per-second))
+        audio-buffer (.createBuffer audio-ctx num-channels 
+                                    (* frames-per-pixel (.-width img-data) (.-height img-data)) 
+                                    (.-sampleRate audio-ctx))]
+    (doseq [ch-i (range num-channels)
+            :let [channel-buffer (.getChannelData audio-buffer ch-i)]]
+      (doseq [px-i (range (* (.-width img-data) (.-height img-data)))
+              :let [px-v (aget (.-data img-data) (* 4 px-i))
+                    au-v (- (* (/ px-v 255) 2) 1)]]
+        (.fill channel-buffer au-v 
+               (* px-i frames-per-pixel)
+               (* (inc px-i) frames-per-pixel))))
+    (js/console.log img-data)
+    (js/console.log audio-buffer)
+    audio-buffer))
+
+(defn img-data->audio
+  [audio-ctx img-data]
+  (let [audio-data (image-array-to-audio-array (.-data img-data))
+        audio-buffer (.createBuffer audio-ctx 2 (.-length audio-data) (.-sampleRate audio-ctx))]
+    (.copyToChannel audio-buffer audio-data 0)
+    (.copyToChannel audio-buffer audio-data 1)
+    (js/console.log audio-buffer)
+    audio-buffer))
+
+(defn play-canvas
+  [canvas]
+  (let [img-data (.getImageData (.getContext canvas "2d") 0 0 (.-width canvas) (.-height canvas))
+        audio-ctx (js/window.AudioContext.)
+        audio-buffer (img-data->audio audio-ctx img-data)
+        src (.createBufferSource audio-ctx)]
+    (set! (.-buffer src) audio-buffer)
+    (.connect src (.-destination audio-ctx))
+    (.start src)))
 
 (defn draw-img
   [canvas img]
@@ -37,8 +105,8 @@
         (aset output-a (+ i 0) avg)
         (aset output-a (+ i 1) avg)
         (aset output-a (+ i 2) avg)
-        (aset output-a (+ i 3) a)))
-    
+        (aset output-a (+ i 3) (js/Math.min a 254))))
+
     (js/ImageData. output-a (.-width img-data) (.-height img-data))))
 
 (defn store-source-img
@@ -73,7 +141,7 @@
 (defn update-crop
   [canvas e]
   (let [{:keys [x y]} (get-cursor-position canvas e)]
-    (swap! crop-region assoc 
+    (swap! crop-region assoc
            :width (- x (:x @crop-region))
            :height (- y (:y @crop-region)))))
 
@@ -96,15 +164,15 @@
                           (draw-img @dom-node @source-image)
                           (when @crop-region
                             (draw-rect @dom-node @crop-region))))]
-    
-    (reagent/create-class 
+
+    (reagent/create-class
      {:component-did-update actual-render
-      
-      :component-did-mount 
-      (fn [this] 
+
+      :component-did-mount
+      (fn [this]
         (reset! dom-node (reagent/dom-node this))
         (actual-render))
-      
+
       :reagent-render
       (fn []
         @crop-region
@@ -121,25 +189,26 @@
                                    (not= 0 (* (:width @crop-region) (:height @crop-region))))
                           (->> @crop-region
                                (img->img-data @source-image)
-                               (desaturate)
+                              ;  (desaturate)
                                (draw-img-data @dom-node))))]
-    
+
     (reagent/create-class
      {:component-did-update actual-render
-      
+
       :component-did-mount
       (fn [this]
         (reset! dom-node (reagent/dom-node this))
         (actual-render))
-      
+
       :reagent-render
       (fn []
         @source-image
         [:canvas#preview-canvas (when-let [{:keys [width height]} @crop-region]
                                   {:width (js/Math.abs width)
-                                   :height (js/Math.abs height)})])})))
+                                   :height (js/Math.abs height)
+                                   :on-click #(play-canvas @dom-node)})])})))
 
-(defn app 
+(defn app
   []
   [:div#app
    [:input#upload {:type "file" :accept "image/*" :on-change store-source-img}]
